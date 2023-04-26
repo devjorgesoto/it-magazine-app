6126f6b6b527569ea4c57eb5dd4fa0f7827e9ef2
devjorgesoto <dev.jorgesoto@gmail.com>
2023-04-26 10:24:30 -0800

    First templates and basic views

--- itmagazineapp/articles/views.py
+++ itmagazineapp/articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+
+    context = {}
+    return render (request, "home.html" , context)
+
+
+def article(request):
+    context = {}
+    return render (request, "article.html" , context )

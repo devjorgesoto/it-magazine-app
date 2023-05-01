b94725541287a4ac4346be0fdc11472fe2bf2df0
devjorgesoto <dev.jorgesoto@gmail.com>
2023-04-30 20:05:59 -0800

    Templates added (profile,read,signup,topnav and workspace), dinamic routes and dinamic views added or improved

--- itmagazineapp/articles/urls.py
+++ itmagazineapp/articles/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
 
+    path("signup/", views.sign_up, name="signup"),
+    path("read/<int:article_id>", views.read, name="read"),
     path("workspace/", views.workspace, name="workspace"),
-    path("profile/", views.profile, name="profile"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
     path("" , views.home, name="home")
 
 ]
--- itmagazineapp/articles/views.py
+++ itmagazineapp/articles/views.py
@@ -4,6 +4,23 @@ from django.http import HttpResponse
 from .forms import UserForm, ArticleForm
 from .models import User, Article
 
+def profile(request, user_id):
+    
+    profile_obj = User.objects.get(id=user_id)
+
+    context = {'profile_obj':profile_obj}
+    return render (request,"profile.html", context)
+    
+
+def read(request ,article_id):
+    
+    article_obj= Article.objects.get(id=article_id)
+    
+    #print (article_obj.description)
+    
+    context = {'article_obj' : article_obj }
+    return render (request, "read.html", context)
+
 def workspace (request):
     
     form_article = ArticleForm(request.POST or None)
@@ -14,7 +31,7 @@ def workspace (request):
     context = {'form_article':form_article }
     return render (request, "workspace.html", context )   
 
-def profile (request):
+def sign_up(request):
     
     form_user = UserForm(request.POST or None)
 
@@ -22,12 +39,12 @@ def profile (request):
       form_user.save()   
 
     context = {'form_user':form_user }
-    return render (request, "profile.html", context )
+    return render (request, "sign_up.html", context )
 
 def home(request):
 
-    article_result = Article.objects.all()
+    article_obj = Article.objects.all()
 
-    context = {'article_result' : article_result}
+    context = {'article_obj' : article_obj}
 
     return render(request, 'home.html', context)
